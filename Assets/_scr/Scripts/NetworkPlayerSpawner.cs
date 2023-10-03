b1a371cb80c322accdf87c1ffe7932972a7bba74
CrisBlu <55038118+CrisBlu@users.noreply.github.com>
2023-10-03 15:01:59 -0400

    Multiplayer Kind of

---
+++ Assets/_scr/Scripts/NetworkPlayerSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    private GameObject playerBody;
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        playerBody = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(playerBody);
+    }
+}

435f4fbc864a0053c012b3c4706b475daa292f34
Alexdelg3 <alexdelg3@gmail.com>
2023-10-05 14:42:09 -0400

    Finish Spawning basis
    Finished spawning scripts

--- Assets/RandomObjectSpawner.cs
+++ Assets/RandomObjectSpawner.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class RandomObjectSpawner : MonoBehaviour
 {
-    public GameObject[] myObjects;
+    //Enemies to spawn
+    [SerializeField]
+    private GameObject Zombie;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))//Upon pressing spacebar, an asset (cylinder) should spawn
-        {
-            int randomIndex = Random.Range(0, myObjects.Length);//Link the random spawn positions to the Position and Scale of the Spawner, also make more spawners.
-            //                                                  This way, the spawning will be linked to the area of the boxes rather than the hardcoded random values down there vv
-            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
-            //                                        Vector(x,y,z)
+    //Spawn intervals
+    [SerializeField]
+    private float zombieInterval = 5f;
 
-            Instantiate(myObjects[randomIndex], randomSpawnPosition, Quaternion.identity);
-        }
-        
+    void Start()
+    {//Each enemy needs a coroutine (interval, enemyName)
+        zombieInterval = Random.Range(10f, 20f);
+        StartCoroutine(spawnEnemy(zombieInterval, Zombie));
     }
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+
 }

391606ccf475f6efbeb8f78decc7fd18bd7362c7
CrisBlu <danielsantana01189@gmail.com>
2023-10-05 16:37:17 -0400

    Player Networking and Grabbing Objects

---
+++ Assets/XRNetworkGrabInteractable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable), typeof(PhotonView), typeof(PhotonRigidbodyView))]
+public class XRNetworkGrabInteractable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private PhotonView photonView;
+
+    void Start()
+    {
+        GetComponent<XRGrabInteractable>().selectEntered.AddListener(OnSelectEnter);
+        photonView = GetComponent<PhotonView>();
+        photonView.OwnershipTransfer = OwnershipOption.Takeover;
+    }
+
+    // Update is called once per frame
+    protected void OnSelectEnter(SelectEnterEventArgs args) {
+        photonView.RequestOwnership();
+    }
+}

835075b9e80a549e0a934fdcfde8da75e4390c9f
CrisBlu <danielsantana01189@gmail.com>
2023-10-10 15:03:21 -0400

    Added UI

---
+++ Assets/WristUI.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WristUI : MonoBehaviour
+{
+    public InputActionAsset inputActions;
+
+    private Canvas _wristUICanvas;
+    private InputAction _menu;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _wristUICanvas = GetComponent<Canvas>();
+        _menu = inputActions.FindActionMap("XRI LeftHand").FindAction("Menu");
+        _menu.Enable();
+        _menu.performed += ToggleMenu;
+    }
+
+    private void OnDestroy()
+    {
+        _menu.performed -= ToggleMenu;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        _wristUICanvas.enabled = !_wristUICanvas.enabled;
+    }
+}

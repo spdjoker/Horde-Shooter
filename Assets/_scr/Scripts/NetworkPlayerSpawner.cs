c70531ed21aa0ff6e973659ba52691ff566554af
CrisBlu <55038118+CrisBlu@users.noreply.github.com>
2023-10-12 14:31:57 -0400

    dw about it

--- Assets/_scr/Scripts/NetworkPlayerSpawner.cs
+++ Assets/_scr/Scripts/NetworkPlayerSpawner.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
+    public Vector3[] spawnLocation;
+    private int spawnIndex = 0;
     private GameObject playerBody;
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
-        playerBody = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+        playerBody = PhotonNetwork.Instantiate("Network Player", spawnLocation[spawnIndex], transform.rotation);
+        spawnIndex++;
     }
 
     public override void OnLeftRoom()

cc981d641228e450a3969d8a53067e37ea59bdd1
Alexdelg3 <alexdelg3@gmail.com>
2023-10-26 15:09:09 -0400

    Potentially finished Spawner's script.

--- Assets/RandomObjectSpawner.cs
+++ Assets/RandomObjectSpawner.cs
@@ -9,7 +9,7 @@ public struct MonsterData {
 public class RandomObjectSpawner : MonoBehaviour
 {
     //Enemies to spawn
-    [SerializeField] private List <MonsterData> mobs;
+    [SerializeField] private List <MonsterData> mobs = new List<MonsterData>(5);
     [SerializeField] private float randomIntervalRange = 1f;
     [SerializeField] private float spawnPositionVariance = 3f;
 
@@ -18,26 +18,36 @@ public class RandomObjectSpawner : MonoBehaviour
 
     //THIS CODE HAS NOT BEEN TESTED. BEWARE.
 
-    float intervalRandomizer(){
-        float range = Random.Range(0, 101);
-        if(range < 25){
-            return mobs[0].spawnInterval;
-        }else if (range < 75){
-            return mobs[1].spawnInterval;
-        }else {//Else range < 100
-            return mobs[2].spawnInterval;
+    float intervalRandomizer() {
+        if (mobs.Count >= 3) {
+            float range = Random.Range(0, 101);
+            if (range < 25) {
+                return mobs[0].spawnInterval;
+            } else if (range < 75) {
+                return mobs[1].spawnInterval;
+            } else {
+                return mobs[2].spawnInterval;
+            }
+        } else {
+            return 0.0f;
         }
     }
-    GameObject enemyRandomizer(){
-        float range = Random.Range(0, 5);
-        if(range == 0){
-            return mobs[0].prefab;
-        }else if (range == 1){
-            return mobs[1].prefab;
-        }else {//Else range is 2, 3 or 4
-            return mobs[2].prefab;
+    GameObject enemyRandomizer() {
+        if (mobs.Count >= 3) {
+            float range = Random.Range(0, 5);
+            if (range == 0) {
+                return mobs[0].prefab;
+            } else if (range == 1) {
+                return mobs[1].prefab;
+            } else {
+                return mobs[2].prefab;
+            }
+        } else {
+            // Handle the case where there are not enough elements in the list.
+            return null; // You can return null or handle the error as needed.
         }
     }
+    
     void Start()
     {//Each enemy needs a coroutine (interval, enemyName)
         StartCoroutine(SpawnEnemy(RandomInterval(), enemyRandomizer()));
